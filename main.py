f5cc4af526ce20275b01b6e1df4a8991d2d1938b
Soft Andowetto <aminrodrigues@outlook.com>
2026-07-28 17:01:25 +0100

    I am too indecisive

--- main.py
+++ main.py
@@ -148,9 +148,6 @@ def _preflight_checks(manifest_data, domain=None, dc_ip=None,
             print(f"    then append it to /etc/hosts:")
             print(f"")
             print(f"        nxc smb {dc_ip} -u '{username}' -p '{password}' --generate-hosts-file hosts.txt | sudo tee -a /etc/hosts < hosts.txt")
-            print(f"")
-            print(f"    or just add a single line for the DC: ")
-            print(f"        echo '{dc_ip} {domain}' | sudo tee -a /etc/hosts")
             ok = False
 
     return ok
